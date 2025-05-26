43f266ac97c691fedbac4640ed9d8ade0af393fe
Kingsword <kingsword09@gmail.com>
2025-05-26 14:08:53 +0800

    fix run error

--- lib/binary.ts
+++ lib/binary.ts
@@ -1,25 +1,29 @@
 /**
  * A binary installer for wasm-pack
- * 
+ *
  * @module
  */
 
 import { Binary } from "@kingsword/denokit/binary-install";
-import * as path from "@std/path";
 import os from "node:os";
-import denoJson from "../deno.json" with {type: "json"}
+import denoJson from "../deno.json" with { type: "json" };
 
 const windows = "x86_64-pc-windows-msvc";
-const __dirname = path.dirname(path.fromFileUrl(import.meta.url))
 
-type ARCH = "x86_64-pc-windows-msvc" | "x86_64-unknown-linux-musl" | "aarch64-unknown-linux-musl" | "x86_64-apple-darwin"
+type ARCH =
+  | "x86_64-pc-windows-msvc"
+  | "x86_64-unknown-linux-musl"
+  | "aarch64-unknown-linux-musl"
+  | "x86_64-apple-darwin";
 
 const getPlatform = (): ARCH => {
   const type = os.type();
   const arch = os.arch();
 
   // https://github.com/nodejs/node/blob/c3664227a83cf009e9a2e1ddeadbd09c14ae466f/deps/uv/src/win/util.c#L1566-L1573
-  if ((type === "Windows_NT" || type.startsWith("MINGW32_NT-")) && arch === "x64") {
+  if (
+    (type === "Windows_NT" || type.startsWith("MINGW32_NT-")) && arch === "x64"
+  ) {
     return windows;
   }
   if (type === "Linux" && arch === "x64") {
@@ -40,8 +44,17 @@ const getBinary = (): Binary => {
   const version = denoJson.version;
   const author = "rustwasm";
   const name = "wasm-pack";
-  const url = `https://github.com/${author}/${name}/releases/download/v${version}/${name}-v${version}-${platform}.tar.gz`;
-  return new Binary(platform === windows ? "wasm-pack.exe" : "wasm-pack", url, version);
+  const url =
+    `https://github.com/${author}/${name}/releases/download/v${version}/${name}-v${version}-${platform}.tar.gz`;
+  const tempDirPath = Deno.makeTempDirSync({
+    prefix: "denokit_binary_install_",
+  });
+  return new Binary(
+    platform === windows ? "wasm-pack.exe" : "wasm-pack",
+    url,
+    version,
+    { installDirectory: tempDirPath },
+  );
 };
 
 /**
@@ -58,9 +71,6 @@ const install = (): void => {
 const run = (): void => {
   const binary = getBinary();
   binary.run();
-}
-
-export {
-  install,
-  run,
 };
+
+export { install, run };
